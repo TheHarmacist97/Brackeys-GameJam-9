f4366465e23969f35a06330bbd63499871f986dc
Varun A. Vyas <varunvyas55@gmail.com>
2023-02-13 12:39:54 +0530

    Target tracking with falloff implemented

--- Brackeys-GameJam2023/Assets/Scripts/Enemy.cs
+++ Brackeys-GameJam2023/Assets/Scripts/Enemy.cs
@@ -1,8 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    
+    public enum EnemyState
+    {
+        CHASE,
+        ATTACK
+    }
+    [SerializeField] private CharacterSpecs specs;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private EnemyState enemyState;
+    [SerializeField] private Transform target;
+
+    private Trigger outsideFalloffRangeTrigger;
+    private float distanceToPlayer;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Init();
+        outsideFalloffRangeTrigger = new Trigger(()=>StartAttack());
+    }
+
+    private void Init()
+    {
+        agent.acceleration = specs.accel;
+        agent.angularSpeed = specs.rotateSpeed;
+        agent.speed = specs.maxMoveSpeed;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        TrackDistance();
+    }
+
+    private void TrackDistance()
+    {
+        distanceToPlayer = (transform.position - target.position).sqrMagnitude;
+        if (distanceToPlayer > specs.FalloffRangeSquare)
+        {
+            outsideFalloffRangeTrigger.Reset();   
+            if (distanceToPlayer > specs.EffectiveRangeSquare)
+            {
+                GiveChase();
+            }
+        }
+        else if (distanceToPlayer < specs.FalloffRangeSquare)
+        {
+            if(distanceToPlayer < specs.EffectiveRangeSquare)
+            {
+                outsideFalloffRangeTrigger.Fire();
+            }
+        }
+    }
+
+    private void GiveChase()
+    {
+        enemyState = EnemyState.CHASE;
+        agent.destination = target.position;
+    }
+
+    private void StartAttack()
+    {
+        agent.ResetPath();
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(target.position, Vector3.up, specs.falloffRange);
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(target.position, Vector3.up, specs.effectiveRange);
+        
+    }
+
 }
---
+++ Brackeys-GameJam2023/Assets/Scripts/Utils/Trigger.cs
@@ -0,0 +1,48 @@
+using System;
+/// <summary>
+/// Class which fires a function once only until it has been reset
+/// </summary>
+public class Trigger
+{
+    public bool fire { get; private set; }
+
+    private Action func;
+    private Action resetFunc;
+
+    /// <summary>
+    /// Set the function
+    /// </summary>
+    /// <param name="func">List a function to trigger as an Action</param>
+    public Trigger(Action func, Action resetFunc = null)
+    {
+        this.func = func;
+        this.resetFunc = resetFunc ?? Dummy;
+        fire = false;
+    }
+
+    /// <summary>
+    /// Fire the Trigger
+    /// </summary>
+    public void Fire()
+    {
+        if (fire) return;
+        fire = true;
+        func();
+    }
+
+    /// <summary>
+    /// Reset the Trigger
+    /// </summary>
+    public void Reset()
+    {
+        if (!fire) return;
+        fire = false;
+        resetFunc();
+    }
+
+    //Simple dummy function which returns.
+    private void Dummy()
+    {
+        return;
+    }
+}
