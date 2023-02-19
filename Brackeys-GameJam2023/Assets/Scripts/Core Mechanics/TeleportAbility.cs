67ca89812d7e9cccad77a150f6c203bb9ef67930
Varun A. Vyas <varunvyas55@gmail.com>
2023-02-19 12:41:18 +0530

    TeleportAbility polished

--- Brackeys-GameJam2023/Assets/Scripts/Core Mechanics/TeleportAbility.cs
+++ Brackeys-GameJam2023/Assets/Scripts/Core Mechanics/TeleportAbility.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class TeleportAbility : InputAbility
 {
-    [SerializeField]
-    private float range;
-    [SerializeField]
-    private float coolDown;
+    private readonly float range = 5f;
+
+    private float coolDown = 5f;
     private bool canTeleport = true;
     private CharacterController controller;
-    private Material baseBodyMat;
-    private Material lineBodyMat;
+    private Material[] materials;
     private void OnEnable() => Initialise();
 
     private void OnDisable()
     {
         QuickTimeEvent.Instance.hijackStarted -= StartedHijacking;
         QuickTimeEvent.Instance.hijackComplete -= CompletedHijacking;
+    }
+
+    private void Awake()
+    {
+        materials = transform.GetChild(0).GetChild(3).GetChild(0).GetComponent<Renderer>().sharedMaterials;
     }
 
     private void Initialise()
@@ -29,9 +32,33 @@ public class TeleportAbility : InputAbility
     }
     public override void Fire(Vector3 target)
     {
+        Vector3 destination;
         if (canTeleport)
         {
-            StartCoroutine(Teleport(transform.position + transform.forward * 5f));
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, range, GameConfig.Constants.GROUND_TAG_INDEX))
+            {
+                destination = hit.point;
+            }
+            else
+            {
+                destination = transform.position + transform.forward * range;
+            }
+            StartCoroutine(Teleport(destination));
+        }
+    }
+
+    public IEnumerator ChangeMaterialProperty(float target, float time, params Material[] materials)
+    {
+        float elapsedTime = 0f;
+        float start = materials[0].GetFloat("_TeleportValue");
+        while (elapsedTime < time)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            foreach (Material mat in materials)
+            {
+                mat.SetFloat("_TeleportValue", Mathf.Lerp(start, target, elapsedTime / time));
+            }
         }
     }
 
@@ -51,16 +78,28 @@ public class TeleportAbility : InputAbility
     }
     private IEnumerator Teleport(Vector3 destination)
     {
+        yield return StartCoroutine(ChangeMaterialProperty(1f, 0.2f, materials));
         canTeleport = false;
         transform.position = destination;
-        yield return new WaitForSeconds(coolDown);
+        StartCoroutine(ChangeMaterialProperty(0f, 0.75f, materials[0]));
+        yield return StartCoroutine(ChangeMaterialProperty(0f, coolDown, materials[1]));
         canTeleport = true;
     }
 
     public void TeleportBack()
     {
-        Vector3 destination = transform.position + (transform.up * 3f);
+        Vector3 destination;
+
+        if (Physics.Raycast(transform.position, transform.up, out RaycastHit hit, range, GameConfig.Constants.GROUND_TAG_INDEX))
+        {
+            destination = hit.point;
+        }
+        else
+        {
+            destination = transform.position + transform.up * range;
+        }
         StartCoroutine(Teleport(destination));
+
     }
 
     private void CompletedHijacking(bool result)
