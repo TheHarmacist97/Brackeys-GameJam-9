8886431e47885bf1977d5603519c397d03b24550
Varun A. Vyas <varunvyas55@gmail.com>
2023-02-19 07:21:54 +0530

    Update KingOfTheHillMarker.cs

--- Brackeys-GameJam2023/Assets/KingOfTheHillMarker.cs
+++ Brackeys-GameJam2023/Assets/KingOfTheHillMarker.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class KingOfTheHillMarker : MonoBehaviour
 {
-    
+    Transform mainCam;
+    private void Awake()
+    {
+        mainCam = Camera.main.transform;
+    }
+
+    public void Update()
+    {
+        transform.position = transform.position + Vector3.up * Mathf.Sin(Time.time*2f)*0.03f;
+        transform.LookAt(mainCam);
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.z = -90f;
+        transform.rotation = Quaternion.Euler(rot);
+    }
 }
